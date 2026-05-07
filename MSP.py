d1af8f39bdb426cf5cbf67326cab34c91976e4a5
Ryutaro-Matsuda <02ryu28@gmail.com>
2026-05-07 21:05:55 +0900

    Add Data Augmentation

--- MSP.py
+++ MSP.py
@@ -17,6 +17,13 @@ color_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
 ])
+train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
+])
 #IDデータ
 # CIFAR-100
 cifar100_train = datasets.CIFAR100(root='/workspace/data', train=True, download=True, transform=color_transform)
