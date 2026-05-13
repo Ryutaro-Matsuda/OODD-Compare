9430128b0afca23d25ab5c1d252d2b00a937f2ad
Ryutaro-Matsuda <02ryu28@gmail.com>
2026-05-13 14:31:28 +0900

    WideResNet

--- MSP.py
+++ MSP.py
@@ -6,6 +6,140 @@ import time
 from sklearn.metrics import roc_auc_score
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader, ConcatDataset, Dataset
+import torch.nn.functional as F
+
+
+class WideBasic(nn.Module):
+    def __init__(self, in_planes, planes, dropout_rate, stride=1):
+        super(WideBasic, self).__init__()
+
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(
+            in_planes,
+            planes,
+            kernel_size=3,
+            padding=1,
+            bias=True
+        )
+
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=True
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=True
+                )
+            )
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.dropout(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += self.shortcut(x)
+        return out
+
+
+class WideResNet(nn.Module):
+    def __init__(self, depth, widen_factor, dropout_rate, num_classes):
+        super(WideResNet, self).__init__()
+
+        self.in_planes = 16
+
+        assert ((depth - 4) % 6 == 0)
+        n = (depth - 4) // 6
+        k = widen_factor
+
+        nStages = [16, 16*k, 32*k, 64*k]
+
+        self.conv1 = nn.Conv2d(
+            3,
+            nStages[0],
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=True
+        )
+
+        self.layer1 = self._wide_layer(
+            WideBasic,
+            nStages[1],
+            n,
+            dropout_rate,
+            stride=1
+        )
+
+        self.layer2 = self._wide_layer(
+            WideBasic,
+            nStages[2],
+            n,
+            dropout_rate,
+            stride=2
+        )
+
+        self.layer3 = self._wide_layer(
+            WideBasic,
+            nStages[3],
+            n,
+            dropout_rate,
+            stride=2
+        )
+
+        self.bn1 = nn.BatchNorm2d(nStages[3])
+        self.linear = nn.Linear(nStages[3], num_classes)
+
+    def _wide_layer(self, block, planes, num_blocks,
+                    dropout_rate, stride):
+
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+
+        for stride in strides:
+            layers.append(
+                block(
+                    self.in_planes,
+                    planes,
+                    dropout_rate,
+                    stride
+                )
+            )
+            self.in_planes = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        out = self.conv1(x)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        out = F.relu(self.bn1(out))
+
+        out = F.avg_pool2d(out, 8)
+
+        out = out.view(out.size(0), -1)
+
+        out = self.linear(out)
+
+        return out
+
 
 gray_transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=3),
@@ -55,11 +189,12 @@ mnist_test = datasets.MNIST(
 )
 test_loader_mnist   = DataLoader(mnist_test, batch_size=64)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-model_ood = models.resnet18(pretrained=True)
-model_ood.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-model_ood.maxpool = nn.Identity()
-model_ood.fc = nn.Linear(model_ood.fc.in_features, 100)
+model_ood = WideResNet(
+    depth=28,
+    widen_factor=10,
+    dropout_rate=0.3,
+    num_classes=100
+)
 model_ood = model_ood.to(device)
 
 def train(model, loader, epochs=200):
