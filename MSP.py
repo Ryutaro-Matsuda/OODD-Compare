70e772056c72681f4e7bd82ec1b7f37eaba6c093
Ryutaro-Matsuda <02ryu28@gmail.com>
2026-05-13 16:30:34 +0900

    Augmentation (WRes)

--- MSP.py
+++ MSP.py
@@ -153,9 +153,8 @@ color_transform = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
 ])
 train_transform = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
+    transforms.RandomResizedCrop(size=imagesize, scale=(0.2, 1.)),
     transforms.RandomHorizontalFlip(),
-    transforms.Resize((32, 32)),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
 ])
